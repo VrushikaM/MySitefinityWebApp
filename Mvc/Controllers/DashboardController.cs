7fccba0d445db5d1a3012c93de9fd2e9e45114d9
VrushikaM <tbs.vrushika@gmail.com>
2024-12-11 14:53:50 +0530

    Initial commit with project files and updated .gitignore

---
+++ Mvc/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace MySitefinityWebApp.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "DashboardWidget", Title = "Dashboard Page Widget", SectionName = "Dashboard Widgets")]
+    public class DashboardController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mvc/Controllers/UsersController.cs
@@ -0,0 +1,120 @@
+﻿using MySitefinityWebApp.Data;
+using System;
+using System.Activities.Expressions;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace MySitefinityWebApp.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "UsersWidget", Title = "Users Page Widget", SectionName = "Users Widgets")]
+    public class UsersController : Controller
+    {
+        private Users_DbEntities db = new Users_DbEntities();
+
+        public ActionResult Index()
+        {
+            var users = db.Users.ToList();
+            return View(users);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "UserId,Name,Email")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "UserId,Name,Email")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            if (!ModelState.IsValid)
+            {
+                foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+            }
+            return View(user);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
